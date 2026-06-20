b2521cecb17cbe8b44e767b975c3acd7ed0cf663
Michael Goodnow <mmgoodnow@gmail.com>
2026-06-19 21:44:39 -0700

    Move iOS app to separate repo

--- ios/FitnessGoals/Sources/Models/GoalSettings.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct GoalSettings: Codable {
-    var yearlyGoalMiles: Double
-    var hrPercentile: Double = 0.999
-
-    static let defaultSettings = GoalSettings(yearlyGoalMiles: 500)
-}
